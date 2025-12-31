e434036a635e4b1e079a67b917213599274ae355
Gopu Vishal <gopuvishal7575@gmail.com>
2025-12-31 13:26:23 +0530

    Update WebConfig.java

--- src/main/java/com/educhain/document_system/config/WebConfig.java
+++ src/main/java/com/educhain/document_system/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
         
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOrigin("https://blockchain-document-storagen-kfym6dhgc.vercel.app");
+        config.addAllowedOrigin("https://blockchain-document-storage.vercel.app/");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         
