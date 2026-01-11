d830e8bd4c51449d91a4861b2a620607ad92b251
Gopu Vishal <gopuvishal7575@gmail.com>
2026-01-11 22:56:45 +0530

    Add VerificationResult class with properties and methods

--- src/main/java/com/educhain/document_system/model/VerificationResult.java
+++ src/main/java/com/educhain/document_system/model/VerificationResult.java
@@ -1,1 +1,62 @@
 
+package com.educhain.document_system.model;
+
+public class VerificationResult {
+    private boolean verified;
+    private String detectedType;
+    private double confidenceScore;
+    private String message;
+    private boolean typeMismatch;
+
+    public VerificationResult() {
+    }
+
+    public VerificationResult(boolean verified, String detectedType, double confidenceScore, String message) {
+        this.verified = verified;
+        this.detectedType = detectedType;
+        this.confidenceScore = confidenceScore;
+        this.message = message;
+        this.typeMismatch = false;
+    }
+
+    // Getters and Setters
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+    public String getDetectedType() {
+        return detectedType;
+    }
+
+    public void setDetectedType(String detectedType) {
+        this.detectedType = detectedType;
+    }
+
+    public double getConfidenceScore() {
+        return confidenceScore;
+    }
+
+    public void setConfidenceScore(double confidenceScore) {
+        this.confidenceScore = confidenceScore;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isTypeMismatch() {
+        return typeMismatch;
+    }
+
+    public void setTypeMismatch(boolean typeMismatch) {
+        this.typeMismatch = typeMismatch;
+    }
+}
